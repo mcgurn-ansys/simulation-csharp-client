cc194c5b8e0dad05c7c1a39cd5ca960ce75dbc35
Tim Sublette <tim.sublette@3dsim.com>
2017-09-28 14:13:27 -0600

    Fix dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM microsoft/aspnetcore-build:2.0.0
 ENV APPLICATION_NAME=simulation-csharp-client
 COPY . /tmp
 WORKDIR /tmp
+ARG key
 
 # Restore main projects
 RUN dotnet restore src/SimulationCSharpClient
@@ -14,7 +15,7 @@ RUN dotnet build --configuration Release src/SimulationCSharpClient
 RUN dotnet pack --no-build --configuration Release src/SimulationCSharpClient
 
 # Publish
-RUN dotnet nuget push src/SimulationCSharpClient/bin/Release/SimulationCSharpClient.0.1.0.nupkg -s https://www.nuget.org/api/v2/package -k <nuget api key>
+RUN dotnet nuget push src/SimulationCSharpClient/bin/Release/SimulationCSharpClient.0.1.0.nupkg -s https://www.nuget.org/api/v2/package -k $key
 
 
 
--- src/SimulationCSharpClient/GlobalSuppressions.cs
+++ src/SimulationCSharpClient/GlobalSuppressions.cs
@@ -2,7 +2,3 @@
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:Element must begin with upper-case letter", Justification = "3DSIM", Scope = "namespace", Target = "~N:_3DSIM.Amazon")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:Element must begin with upper-case letter", Justification = "3DSIM", Scope = "namespace", Target = "~N:_3DSIM.AmazonWrapper")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:Element must begin with upper-case letter", Justification = "3DSIM", Scope = "namespace", Target = "~N:_3DSIM.Amazon.Events")]
