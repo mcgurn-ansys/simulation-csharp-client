062508dd879f1a80e92bccf0718b0974a5895457
Pete Krull <pete.krull@3dsim.com>
2017-10-02 21:20:00 -0600

    Require id in BuildFile. Add build file types.

---
+++ src/SimulationCSharpClient/Client/BuildFileStates.cs
@@ -0,0 +1,11 @@
+namespace SimulationCSharpClient.Client
+{
+    public class BuildFileStates
+    {
+        public const string Uploaded = "Uploaded";
+        public const string Processing = "Processing";
+        public const string Available = "Available";
+        public const string Error = "Error";
+        public const string Archived = "Archived";
+    }
+}
